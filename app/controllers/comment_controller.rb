f0316ba57028c982b7581f99a66de6bc5a748755
Lozamded <dhiguerafernandez@gmail.com>
2018-01-17 09:50:57 -0300

    tercero

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,10 @@
+class CommentController < ApplicationController
+  def index
+    @comments = Comment.all
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20180113032032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "coments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.datetime "created_at", null: false
