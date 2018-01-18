e9ac46875a5ad75934bd5fca3e575b240c8b5cec
Lozamded <dhiguerafernandez@gmail.com>
2018-01-18 01:38:53 -0300

    cuarto commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/comment' => 'comment#index'
 end
